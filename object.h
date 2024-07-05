5657d352b59e7d6744601b0fe8e21ba44e10f35e
Prashanth Pujar <ppujar@usc.edu>
2024-07-04 19:38:25 -0700

    Add support for String using generic Obj Representation
    Motivation
    
    Since, String can grow and shrink, we allocate string in Heap unlike with
    number, bool and nil where we added them to stack using the value.
    
    Here, the value payload is a Obj* which point to data on heap.
    
    Since, we needed a generic representation for data on heap,
    we used obj.

---
+++ object.h
@@ -0,0 +1,41 @@
+#ifndef clox_object_h
+#define clox_object_h
+
+#include "common.h"
+#include "value.h"
+
+#define OBJ_TYPE(value) (AS_OBJ(value)->type)
+
+// When downcasting from Obj --> ObjString, we need to ensure the type is string
+#define IS_STRING(value) isObjType(value, OBJ_STRING)
+
+#define AS_STRING(value) ((ObjString *)AS_OBJ(value))
+#define AS_CSTRING(value) (((ObjString *)AS_OBJ(value))->chars)
+
+typedef enum {
+  OBJ_STRING,
+} ObjType;
+
+struct Obj {
+  ObjType type;
+};
+
+struct ObjString {
+  Obj obj;
+  int length;
+  char *chars;
+};
+
+ObjString *copyString(const char *chars, int length);
+
+// Why not define function itself as a macro?
+// As seen, the body uses "value" twice, and macro is expanded
+// by argument expression every place parameter name appear in the body.
+// Hence, each argument expression is evaluated multiple times.
+// If we pass something which has a side effect say IS_STRING(pop())
+// We will end up executing pop() twice.
+static inline bool isObjType(Value value, ObjType type) {
+  return IS_OBJ(value) && AS_OBJ(value)->type == type;
+}
+
+#endif
