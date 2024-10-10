665566e321823726af90fc0df6a77c6c94b724cc
Prashanth Pujar <ppujar@usc.edu>
2024-10-09 22:25:55 -0700

    Add optimization to perform the super method call
    **Description**
    
    This commit adds changes to optimize the super.method() call.
    Here the stack looks more like below
    
    syn("this") argumentList   syn("super")
    instance   arg1 arg2 argn  superclass
    
    we pop the superclass, then find method/closure in methods
    of that class.
    
    Now with the that, stack already looks like callFrame we usually
    set up.
    
    [instance, arg1, arg2, argn]

--- debug.c
+++ debug.c
@@ -127,6 +127,8 @@ int disassembleInstruction(Chunk *chunk, int offset) {
     return constantInstruction("OP_GET_GLOBAL", chunk, offset);
   case OP_GET_SUPER:
     return constantInstruction("OP_GET_SUPER", chunk, offset);
+  case OP_SUPER_INVOKE:
+    return invokeInstruction("OP_SUPER_INVOKE", chunk, offset);
   case OP_EQUAL:
     return simpleInstruction("OP_EQUAL", offset);
   case OP_GREATER:
